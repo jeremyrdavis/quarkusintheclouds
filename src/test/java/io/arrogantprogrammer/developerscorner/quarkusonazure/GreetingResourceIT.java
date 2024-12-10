6ad04a6c743bb359771d4bccfdff917acaad4389
jeremyrdavis <jeremyrdavis@mac.com>
2024-12-10 13:23:48 -0500

    initial

---
+++ src/test/java/io/arrogantprogrammer/developerscorner/quarkusonazure/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package io.arrogantprogrammer.developerscorner.quarkusonazure;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
